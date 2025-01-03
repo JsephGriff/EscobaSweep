99930247cbedd891635388d62c472e49694415bb
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2025-01-02 18:38:09 -0800

    Out of bounds Card values now throw IllegalArgumentExceptions

--- src/Card.java
+++ src/Card.java
@@ -10,12 +10,12 @@ public class Card
     {
         if(value > MAX_VALUE)
         {
-            throw new RuntimeException("Card value is above Card.MAX_VALUE");
+            throw new IllegalArgumentException("Card value is above Card.MAX_VALUE");
         }
 
         if(value > MIN_VALUE)
         {
-            throw new RuntimeException("Card value is below Card.MIN_VALUE");
+            throw new IllegalArgumentException("Card value is below Card.MIN_VALUE");
         }
 
         this.value = value;
