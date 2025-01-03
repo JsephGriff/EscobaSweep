cc98781ed424acb697a4f0c2a9e68004907551da
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2025-01-02 19:32:18 -0800

    Update GameTest.java

--- test/src/src/GameTest.java
+++ test/src/src/GameTest.java
@@ -1,7 +1,13 @@
 package src;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
-
+    @Test
+    public void testCanConstructGame()
+    {
+        Game g = new Game();
+    }
 }
