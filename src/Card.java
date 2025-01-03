4e8e0c6fc622fde39bbbc2f402ccf22ef066a5c4
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2025-01-02 17:08:52 -0800

    Create Card.java
    Created a card Folder

---
+++ src/Card.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Card
+{
+    public int value;
+
+    public CardSuit suit;
+
+    
+
+    public enum CardSuit
+    {
+        Swords,
+        Cups,
+        Coins,
+        Clubs
+    }
+}
