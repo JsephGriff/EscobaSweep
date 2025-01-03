31354d157b8c08094c8f397fb80799204ce90412
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2025-01-02 21:40:53 -0800

    Added Round Logic
    Added Game.process_round()
    
    Added Game.TARGET_SCORE
    
    Added IView parameter to Game.Game

--- src/Game.java
+++ src/Game.java
@@ -21,12 +21,16 @@ public class Game
 
     long seed;
 
-    public Game()
+    IView user_interface;
+
+    public Game(IView ui)
     {
         deck = new Stack<>();
         seed = Time.valueOf(LocalTime.now()).getTime();
         random = new Random(seed);
         board_cards = new ArrayList<>();
+
+        user_interface = ui;
 
         player_idx = 0;
     }
@@ -35,7 +39,12 @@ public class Game
     {
         players.addAll(player_list);
 
+        for(Player p : players)
+        {
+            p.score = 0;
+        }
 
+        begin_round();
 
     }
 
@@ -49,11 +58,205 @@ public class Game
         for(Player p: players)
         {
             p.clear_hand();
+            p.clear_point_pile();
+            p.round_escobas = 0;
         }
 
         createDeck();
         deal_to_board();
         deal_to_players();
+    }
+
+    public void process_round()
+    {
+        boolean can_play_cards = true;
+
+        for(Player p: players)
+        {
+            can_play_cards = can_play_cards && p.hand.size() != Player.HAND_MAX_SIZE;
+        }
+
+        if(!can_play_cards)
+        {
+            throw new RuntimeException("Cannot begin round. Player's hands are not empty.");
+        }
+
+        while (can_play_cards)
+        {
+            // Turn Setup
+            Player turn_player = players.get(player_idx);
+
+            // All Hands Are Empty
+            if(turn_player.hand.isEmpty())
+            {
+                if (deck.isEmpty())
+                {
+                    can_play_cards = false;
+                    break;
+                } else
+                {
+                    deal_to_players();
+                }
+            }
+
+            user_interface.DisplayPlayerCards((ArrayList<Card>) turn_player.hand);
+            user_interface.DisplayBoardCards(board_cards);
+
+
+            // Take and validate player turn
+            boolean is_valid_turn = false;
+            IView.Play player_turn;
+
+            do
+            {
+                player_turn = user_interface.GetPlay();
+
+                int turn_value = turn_player.hand.get(player_turn.playerCardIndex).value;
+
+                if(player_turn.boardCardIndices.isEmpty())
+                {
+                    is_valid_turn = true;
+                    break;
+                }
+
+                for(int i : player_turn.boardCardIndices)
+                {
+                    turn_value += i;
+                }
+
+                is_valid_turn = turn_value == TARGET_SCORE;
+
+                if(!is_valid_turn)
+                {
+                    user_interface.DisplayErrorMessage("Card Total Doesn't Add to " + TARGET_SCORE);
+                }
+            } while (!is_valid_turn);
+
+
+            // Process Player Turn
+            if(player_turn.boardCardIndices.isEmpty()) // Discard Case
+            {
+                Card discarded_card = turn_player.discard(player_turn.playerCardIndex);
+
+                board_cards.add(discarded_card);
+            } else // Play Case
+            {
+                Card player_card = turn_player.discard(player_turn.playerCardIndex);
+
+                ArrayList<Card> board_take = new ArrayList<>();
+
+                for(int idx: player_turn.boardCardIndices)
+                {
+                    board_take.add(board_cards.get(idx));
+                }
+
+                for(Card c: board_take)
+                {
+                    board_cards.remove(c);
+                    turn_player.point_pile.add(c);
+                }
+
+                turn_player.point_pile.add(player_card);
+
+                // Escoba Case
+                if(board_cards.isEmpty())
+                {
+                    turn_player.round_escobas += 1;
+                }
+            }
+
+            // Close turn
+            ++player_idx;
+
+            if(player_idx >= players.size())
+            {
+                player_idx = 0;
+            }
+        }
+
+        tally_player_points();
+    }
+
+    public void tally_player_points()
+    {
+        int most_coins = -1;
+        int most_sevens = -1;
+        int most_cards = -1;
+
+        ArrayList<Player> top_coins = new ArrayList<>();
+        ArrayList<Player> top_sevens = new ArrayList<>();
+        ArrayList<Player> top_cards = new ArrayList<>();
+
+        for(Player p: players)
+        {
+            p.score += p.round_escobas;
+
+            int count_coins = 0;
+            int count_sevens = 0;
+            int count_cards = 0;
+
+            for(Card c: p.hand)
+            {
+                if(c.value == 7)
+                {
+                    ++count_sevens;
+
+                    if(c.suit == Card.CardSuit.Coins)
+                    {
+                        p.score += 1;
+                    }
+                }
+
+                if(c.suit == Card.CardSuit.Coins)
+                {
+                    ++count_coins;
+                }
+
+                ++count_cards;
+            }
+
+            if(count_sevens > most_sevens)
+            {
+                top_sevens.clear();
+                top_sevens.add(p);
+            } else if(count_sevens == most_sevens)
+            {
+                top_sevens.add(p);
+            }
+
+            if(count_coins > most_coins)
+            {
+                top_coins.clear();
+                top_coins.add(p);
+            } else if(count_coins == most_coins)
+            {
+                top_coins.add(p);
+            }
+
+            if(count_cards > most_cards)
+            {
+                top_cards.clear();
+                top_cards.add(p);
+            } else if(count_cards == most_cards)
+            {
+                top_cards.add(p);
+            }
+        }
+
+        if(top_sevens.size() == 1)
+        {
+            top_sevens.getFirst().score++;
+        }
+
+        if(top_coins.size() == 1)
+        {
+            top_coins.getFirst().score++;
+        }
+
+        if(top_cards.size() == 1)
+        {
+            top_cards.getFirst().score++;
+        }
     }
 
     public void deal_to_board()
@@ -105,4 +308,5 @@ public class Game
         }
     }
 
+    public static int TARGET_SCORE = 15;
 }
